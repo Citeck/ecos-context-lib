5fd99a9a8e83758b39a4488580afc782c869909f
Pavel Simonov <pavel.simonov@citeck.ru>
2023-02-09 10:02:36 +0700

    ECOSCOM-4698 - Develop universal transaction system for ECOS

--- ecos-context-lib/src/main/java/ru/citeck/ecos/context/lib/auth/component/SimpleAuthComponent.kt
+++ ecos-context-lib/src/main/java/ru/citeck/ecos/context/lib/auth/component/SimpleAuthComponent.kt
@@ -2,21 +2,26 @@ package ru.citeck.ecos.context.lib.auth.component
 
 import ru.citeck.ecos.context.lib.auth.data.AuthData
 import ru.citeck.ecos.context.lib.auth.data.EmptyAuth
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SimpleAuthComponent(private val defaultAuth: AuthData = EmptyAuth) : AuthComponent {
 
     private val fullAuth = ThreadLocal.withInitial<AuthData> { EmptyAuth }
     private val runAsAuth = ThreadLocal.withInitial<AuthData> { EmptyAuth }
+    private val customFullAuth = AtomicBoolean(false)
 
     override fun <T> runAs(auth: AuthData, full: Boolean, action: () -> T): T {
 
         if (full) {
             val fullPrev = fullAuth.get()
+            val customAuthBefore = customFullAuth.get()
             try {
+                customFullAuth.set(true)
                 fullAuth.set(auth)
                 return action.invoke()
             } finally {
                 fullAuth.set(fullPrev)
+                customFullAuth.set(customAuthBefore)
             }
         } else {
             if (fullAuth.get().isEmpty()) {
@@ -33,12 +38,10 @@ class SimpleAuthComponent(private val defaultAuth: AuthData = EmptyAuth) : AuthC
     }
 
     override fun getCurrentFullAuth(): AuthData {
-        val fullAuth = fullAuth.get()
-        return if (fullAuth.isEmpty()) {
-            defaultAuth
-        } else {
-            fullAuth
+        if (!customFullAuth.get()) {
+            return defaultAuth
         }
+        return fullAuth.get()
     }
 
     override fun getCurrentRunAsAuth(): AuthData {
